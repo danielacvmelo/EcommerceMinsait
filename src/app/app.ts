cc0a471bf6d498af22f28756d4236fe99f4ede0d
danielacvmelo <danielacvmelo@gmail.com>
2025-11-29 13:38:45 -0300

    feat: add header component with a navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], 
+  imports: [RouterOutlet, Header], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
