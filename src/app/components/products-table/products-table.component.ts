16834a8e330032d5715b01ebc3a1df4816df4e73
danielacvmelo <danielacvmelo@gmail.com>
2025-12-06 10:33:22 -0300

    feat: create admin page with reactive form to edit the products and complete a CRUD

--- src/app/components/products-table/products-table.component.ts
+++ src/app/components/products-table/products-table.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
 
 @Component({
-  selector: 'app-products-table',
+  selector: 'app-product-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-table.component.html',
   styleUrl: './products-table.component.css'
 })
 export class ProductsTableComponent {
+  @Input() products: Product[] = [];
+  @Output() edit = new EventEmitter<Product>();
+  @Output() delete = new EventEmitter<number>();
 
+  onEdit(product: Product) {
+    this.edit.emit(product);
+  }
+
+  onDelete(product: Product) {
+    if (confirm(`Tem certeza que deseja excluir "${product.name}"?`)) {
+      this.delete.emit(product.id);
+    }
+  }
 }
